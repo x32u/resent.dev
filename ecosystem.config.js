4c2332f6ab000aad106f40880c335d59c1e30e53
nonprejudicial <damon@evict.cc>
2024-07-22 18:33:56 +0000

    fix status page

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+      apps: [
+        {
+          name: 'evict.cc',
+          script: 'yarn',
+          args: 'run start',
+          interpreter: 'none', // This tells PM2 not to use Node.js to run the script
+        },
+      ],
+    };
--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -1,168 +1,167 @@
 "use client";
-import { useEffect, useState } from "react";
 import { TbCloudDataConnection } from "react-icons/tb";
 import { MdOutlineTimeline } from "react-icons/md";
 import { ImConnection } from "react-icons/im";
 import { HiServerStack } from "react-icons/hi2";
 import { FaUsers } from "react-icons/fa";
-import Exported from "./shards.json";
 import { IoInformationCircleSharp } from "react-icons/io5";
+import { useEffect, useState } from "react";
 import { Navbar } from "@/components/global/Navbar";
-
+import axios from "axios";
 
 interface ShardData {
-    shard_id: number;
-    is_ready: boolean;
-    server_count: number;
-    cached_user_count: number;
-    uptime: string;
-    latency: number;
+  shard_id: number;
+  uptime: string;
+  latency: number;
+  server_count: number;
+  member_count: number;
 }
 
-const calculateUptime = (unixTimestamp: number): string => {
-    const now = new Date();
-    const uptimeDate = new Date(unixTimestamp * 1000);
-    const diffInSeconds = Math.floor(
-        (now.getTime() - uptimeDate.getTime()) / 1000
-    );
-    const hours = Math.floor(diffInSeconds / 3600);
-    const minutes = Math.floor((diffInSeconds % 3600) / 60);
-    const days = Math.floor(diffInSeconds / 86400);
-
-    if (days > 0) {
-        return `${days} day`;
-    } else if (hours > 0) {
-        return `${hours} hour`;
-    } else {
-        return `${minutes} minute`;
-    }
-};
-
 export default function Status() {
-    const [shards, setShards] = useState<ShardData[]>(
-        Exported.shards.map((shard) => ({
-            ...shard,
-            uptime: calculateUptime(shard.uptime),
-            latency: Math.floor(shard.latency),
-        }))
-    );
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setShards(
-                Exported.shards.map((shard) => ({
-                    ...shard,
-                    uptime: calculateUptime(shard.uptime),
-                    latency: Math.floor(shard.latency),
-                }))
-            );
-        }, 1000);
+  const [shards, setShards] = useState<ShardData[]>([]); // Ensure shards is always an array
 
-        return () => clearInterval(intervalId);
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let config = {
+          method: "get",
+          maxBodyLength: Infinity,
+          url: "https://kure.pl/shards/get",
+          headers: {
+            "api-key": "M!58ZCTj0fTkai%",
+          },
+        };
 
-    return (
-        <>
-            <Navbar />
-            <main className="mt-20 mx-10">
-                <section className="max-w-5xl mx-auto w-full pb-20 pt-20">
-                    <div className="flex flex-row justify-between gap-20">
-                        <div className="flex flex-row items-center gap-2 text-white">
-                            <div className="flex flex-row justify-center items-center w-12 h-12 bg-loti-200 rounded-full">
-                                <TbCloudDataConnection className="w-6 h-6" />
-                            </div>
-                            <h1 className="text-3xl font-bold">Shards</h1>
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-4 mt-10 lg:flex-row">
-                        {shards.map((shard) => (
-                            <Shard
-                                key={shard.shard_id}
-                                shard={shard.shard_id.toString()}
-                                uptime={shard.uptime.toString()}
-                                latency={shard.latency.toString()}
-                                servers={shard.server_count.toString()}
-                                users={shard.cached_user_count.toString()}
-                            />
-                        ))}
-                    </div>
-                </section>
-            </main>
-        </>
-    );
+        const response = await axios.request(config);
+        console.log(JSON.stringify(response.data));
+        setShards(response.data['shards'] || []); // Ensure shards is set to an array
+      } catch (error) {
+        console.error("Error fetching shards:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <main className="mt-20 mx-10">
+      <Navbar />
+      <section className="max-w-5xl mx-auto w-full pb-20 pt-20">
+        <div className="flex flex-row justify-between gap-20">
+          <div className="flex flex-row items-center gap-2 text-white">
+            <div className="flex flex-row justify-center items-center w-12 h-12 bg-loti-200 rounded-full">
+              <TbCloudDataConnection className="w-6 h-6" />
+            </div>
+            <h1 className="text-3xl font-bold">Shards</h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 mt-10 lg:flex-row">
+          {shards.length === 0 ? (
+            <div className="flex items-center justify-center text-white">
+              <p className="text-2xl animate-pulse">
+                Fetching shards information...
+              </p>
+            </div>
+          ) : (
+            shards.map((shard: ShardData) => (
+              <Shard
+                key={shard.shard_id}
+                shard={shard.shard_id.toString()}
+                uptime={shard.uptime}
+                latency={shard.latency}
+                servers={shard.server_count.toString()}
+                users={shard.member_count.toString()}
+              />
+            ))
+          )}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 const Shard = ({
-    shard,
-    uptime,
-    latency,
-    servers,
-    users,
+  shard,
+  uptime,
+  latency,
+  servers,
+  users,
 }: {
-    shard: string;
-    uptime: string;
-    latency: string;
-    servers: string;
-    users: string;
+  shard: string;
+  uptime: string;
+  latency: number;
+  servers: string;
+  users: string;
 }) => {
-    const fServers = Number(servers).toLocaleString();
-    const fUsers = Number(users).toLocaleString();
-    return (
-        <div className="flex flex-col py-6 rounded-3xl bg-loti-200 border transition-shadow duration-200 ease-linear border-loti-300 text-white">
-            <div className="h-full flex flex-col justify-between">
-                <div className="px-6">
-                    <div className="flex items-start justify-between gap-x-4">
-                        <div className="flex items-center gap-2">
-                            <p className="text-xl font-semibold inline-flex items-center">
-                                Shard {shard}
-                            </p>
-                        </div>
-                        <div className="flex justify-center items-center bg-loti-400 border border-loti-300 rounded-xl px-2 py-1 gap-2">
-                            <div className="w-4 h-4 bg-green-500 rounded-full animate-pulse"></div>
-                            <p className="text-sm font-semibold inline-flex items-center text-green-500">
-                                Operational
-                            </p>
-                        </div>
-                    </div>
-                    <div className="flex flex-row gap-2 items-center">
-                        <IoInformationCircleSharp className="text-loti-600" />
-                        <p className="text-sm text-loti-600">
-                            Reloads Automatically
-                        </p>
-                    </div>
-                </div>
-                <hr className="border-t border-loti-300 w-full my-4" />
-                <div className="grid grid-cols-2 gap-4 px-6">
-                    <div className="flex flex-col gap-2">
-                        <p className="text-md text-loti-600">Uptime</p>
-                        <div className="flex flex-row gap-2 items-center">
-                            <MdOutlineTimeline className="text-loti-600" />
-                            <p className="text-md font-semibold">{uptime}</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <p className="text-md text-loti-600">Latency</p>
-                        <div className="flex flex-row gap-2 items-center">
-                            <ImConnection className="text-loti-600" />
-                            <p className="text-md font-semibold">{latency}ms</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <p className="text-md text-loti-600">Servers</p>
-                        <div className="flex flex-row gap-2 items-center">
-                            <HiServerStack className="text-loti-600" />
-                            <p className="text-md font-semibold">{fServers}</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <p className="text-md text-loti-600">Users</p>
-                        <div className="flex flex-row gap-2 items-center">
-                            <FaUsers className="text-loti-600" />
-                            <p className="text-md font-semibold">{fUsers}</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="flex flex-col py-6 rounded-3xl bg-loti-200 border transition-shadow duration-200 ease-linear border-loti-300 text-white">
+      <div className="h-full flex flex-col justify-between">
+        <div className="px-6">
+          <div className="flex items-start justify-between gap-x-4">
+            <div className="flex items-center gap-2">
+              <p className="text-xl font-semibold inline-flex items-center">
+                Shard {shard}
+              </p>
             </div>
+            <div className="flex justify-center items-center bg-loti-400 border border-loti-300 rounded-xl px-2 py-1 gap-2">
+              <div className="w-4 h-4 bg-green-500 rounded-full animate-pulse"></div>
+              <p className="text-sm font-semibold inline-flex items-center text-green-500">
+                Operational
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row gap-2 items-center">
+            <IoInformationCircleSharp className="text-loti-600" />
+            <p className="text-sm text-loti-600">Refresh For Updates</p>
+          </div>
         </div>
-
-    );
+        <hr className="border-t border-loti-300 w-full my-4" />
+        <div className="grid grid-cols-2 gap-4 px-6">
+          <div className="flex flex-col gap-2">
+            <p className="text-md text-loti-600">Uptime</p>
+            <div className="flex flex-row gap-2 items-center">
+              <MdOutlineTimeline className="text-loti-600" />
+              <p className="text-md font-semibold">
+                {uptime
+                  .replaceAll("days", "d")
+                  .replaceAll("day", "d")
+                  .replaceAll("hours", "hrs")
+                  .replaceAll("hour", "hr")
+                  .replaceAll("minutes", "m")
+                  .replaceAll("minute", "m")
+                  .replace("seconds", "s")
+                  .replace("second", "s")
+                  .replaceAll(" ", "")
+                  .replaceAll("and", " ")
+                  .replaceAll(",", " ")}
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-md text-loti-600">Latency</p>
+            <div className="flex flex-row gap-2 items-center">
+              <ImConnection className="text-loti-600" />
+              <p className="text-md font-semibold">
+                {Math.floor(latency * 1000)}ms
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-md text-loti-600">Servers</p>
+            <div className="flex flex-row gap-2 items-center">
+              <HiServerStack className="text-loti-600" />
+              <p className="text-md font-semibold">{servers}</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-md text-loti-600">Users</p>
+            <div className="flex flex-row gap-2 items-center">
+              <FaUsers className="text-loti-600" />
+              <p className="text-md font-semibold">{users}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
