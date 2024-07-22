4f612c4a7b5e54f62c0778432d47fb10870b2fb9
nonprejudicial <damon@evict.cc>
2024-07-22 19:41:07 +0000

    add status to navbar

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -16,6 +16,7 @@ export const Navbar = () => {
                     <Link href="/commands" className={`font-semibold py-6 ${pathname === '/commands' ? 'border-b-2 border-loti-pink text-loti-white' : ''}`}>Commands</Link>
                     <Link href="/docs" className={`font-semibold py-6 ${pathname === '/docs' ? 'border-b-2 border-loti-pink text-loti-white' : ''}`}>Documentation</Link>
                     <Link href="/legal" className={`font-semibold py-6 ${pathname === '/docs' ? 'border-b-2 border-loti-pink text-loti-white' : ''}`}>Legal</Link>
+                    <Link href="/status" className={`font-semibold py-6 ${pathname === '/status' ? 'border-b-2 border-loti-pink text-loti-white' : ''}`}>Status</Link>
                 </div>
                 <Link href='https://discord.gg/evict' className="flex items-center bg-blue-500 px-4 rounded-2xl gap-2 h-10 mt-2 hover:bg-blue-700 sm:mt-4">
                     <BsDiscord />
